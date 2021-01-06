1bff5d51789ec823bb3f931597098b376b293855
Shi Hongyu <2071114140@qq.com>
2021-01-06 11:32:37 +0800

    add ajax

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
 import { App } from './App';
 import store from './app/store';
 import { todoListActions } from './features/todoList/actions/todoListActions';
+import  Accessor  from './HttpAccessor/Accessor';
 
 const root = document.getElementById('root');
 root ? render(
@@ -14,8 +14,6 @@ root ? render(
 , root) : false;
 
 initList();
-
-import  Accessor  from './HttpAccessor/Accessor';
 
 async function initList() {
   const accessor = new Accessor('http://localhost:8080');
